27f6d06b02f1062702632a96c1d885dca01be93a
Balazs Kiss <kissbalazs1983@gmail.com>
2020-04-25 10:30:57 +0200

    JS practices

---
+++ script.js
@@ -0,0 +1,77 @@
+
+/*for (let i = 0; i < 10; i = i + 2) {
+    console.log(i);
+  }
+  */
+/*let oneNumber = 10;
+
+  for (let i =  0; i < oneNumber; i++) {
+      if (i%2 === 0)
+      console.log(i);
+  }*/
+
+  /*let add = (a, b) => {
+    return a + b;  // the return keyword will give you back the result
+  };
+  
+  console.log(add(3, 2)) // will print 5*/
+
+/*let printNumbers = (n) => {
+
+  for (let i = 0; i < n; i++) {
+    console.log(i);
+  }
+};
+console.log(printNumbers(10))*/
+/*
+let greetingTo = (names) => {
+    return ("Hello " + names);
+}
+console.log(greetingTo('Bazsi'))
+
+*/
+
+/*
+
+let printValues = (array) => {
+    for (let i = 0 ; i < array.length; i++) {
+        console.log(array[i]);
+    }
+};
+printValues([0, 3, 6, 7, 10]);
+
+
+//majd egyszer rajovok miert nem jo
+
+
+*/
+
+
+
+/*
+
+let myNumbers = [10, 20, 30, 50, 12];
+
+myNumbers.forEach((number) => {
+    console.log(number);
+});
+
+*/
+
+/*
+let printValues = [0, 3, 6, 7, 10];
+
+printValues.forEach((array) => {
+    console.log(array);
+});
+*/
+/*
+let printValues2 = (array) => {
+    array.forEach(value => {
+      console.log(value);
+    });
+  }
+  printValues2 ([0, 3, 7]);
+*/
+
+
